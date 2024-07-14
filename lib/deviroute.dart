57a32c30b9c7912f8b540947ab4e67a5c540bec4
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2024-07-15 00:47:35 +0530

    v1.0.0

--- lib/deviroute.dart
+++ lib/deviroute.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
+/// A utility class to manage navigation between pages in a Flutter app.
 class DeviRoute {
-  route(context, page) {
+  /// Navigates to the specified [page] using the given [context].
+  ///
+  /// [context] is the BuildContext for navigation.
+  /// [page] is the widget to navigate to.
+  void route(BuildContext context, Widget page) {
     Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => page,
-        ));
+      context,
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
   }
 
-  back(context) {
+  /// Pops the current page off the navigation stack using the given [context].
+  ///
+  /// [context] is the BuildContext for navigation.
+  void back(BuildContext context) {
     Navigator.pop(context);
   }
 }
